6a2054a1d6a4457d4cf93afd413820a77258258b
Michał Zalewski <78042518+MichalZal@users.noreply.github.com>
2022-07-10 23:47:20 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,7 @@ import {configureStore} from '@reduxjs/toolkit'
 import uiSlice from './ui-slice'
 import cartSlice from './cart-slice'
 
+// inaczej create store 
 const store = configureStore({
   reducer: {
     ui: uiSlice.reducer,
